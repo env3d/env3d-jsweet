3455c869e2d26689247e47d18424adba5a45fd42
Jason Madar <jason@vandrico.com>
2018-10-19 21:29:52 -0700

    switch to the transpiler cluster

--- transpile.js
+++ transpile.js
@@ -15,8 +15,8 @@ function transpile(javaCode) {
     formData.append('tid', uniqueId);
     
     return new Promise( (resolve, reject) => {
-        let transpile_host = 'https://api.operatoroverload.com/jsweet/transpile';
-        //let transpile_host = 'http://transpile.c3d.io/transpile';
+        //let transpile_host = 'https://api.operatoroverload.com/jsweet/transpile';
+        let transpile_host = 'https://transpile.c3d.io/transpile';
         fetch(transpile_host,
               {
                   method: 'POST',
